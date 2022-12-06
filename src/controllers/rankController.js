2a271783f276c72917e2b5d5ae15cb30b7eb55a0
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-12-07 00:09:13 +0900

    [ADD] rankList, addRank

--- src/controllers/rankController.js
+++ src/controllers/rankController.js
@@ -1,14 +1,39 @@
 import db from "../db"
 
 export const rankList = async(req,res) => {
-    db.query(`select * from Rank`, (err,result) => {
+    console.log(req.params.map)
+    const _map = req.params.map
+    db.query(`select * from RankTable where map = '${_map}'`, (err,result) => {
+        if (err) console.error(err)
 
+        console.log(result)
+        return res.status(200).json({
+            code:200,
+            result
+        })
     })
 }
 
 export const addRank = async(req,res) => {
-    const {name,time} = req.body
+    const {name,time,map} = req.body
     console.log("name",name)
     console.log("time",time)
-    // db
+    console.log("map",map)
+        
+        try{
+    db.query(`insert into RankTable(name,time,map) value('${name}','${time}','${map}')`,(err,result) => {
+        console.log(result)
+            return res.status(201).json({
+                status:201,
+                Message: '성공적으로 등록했습니다',
+            })
+        }
+        
+    )}catch(err){
+        return res.status(500).json({
+            status: 500,
+            errMessage: '기록 생성 실패',
+        })
+    }
+
 }
--- src/routers/rankRouter.js
+++ src/routers/rankRouter.js
@@ -4,7 +4,7 @@ import {rankList,addRank} from "../controllers/rankController"
 const rankRouter = express.Router()
 
 // 이름, 시간(기록)
-rankRouter.get('/ranklist',rankList)
+rankRouter.get('/ranklist/:map',rankList)
 rankRouter.post('/addrank',addRank)
 
 export default rankRouter
